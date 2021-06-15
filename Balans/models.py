5023bdccbcaeb52e00f6b70a1cc2fc491ccb0401
faust <fanosport1@gmail.com>
2021-06-15 18:04:08 +0300

    add city and weather

--- Balans/models.py
+++ Balans/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
-
     def __str__(self):
         return str(self.user)
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+
--- Balans/views.py
+++ Balans/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import requests
 
 from .forms import Userregistarions
 
 @login_required
 def index(request):
-    print(request)
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=3b212a2a8d80257d845ca625a25c1522'
+    city = 'las Vegas'
+
+    city_weather = requests.get(url.format(city)).json()
+
+    print(city_weather)
     return render(request, 'user/index.html')
 
 def register(request):
