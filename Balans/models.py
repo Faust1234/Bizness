2dd2e0363febd98b13771d3a83dc5fce34ec6495
faust <fanosport1@gmail.com>
2021-06-16 10:52:23 +0300

    all weather

--- Balans/models.py
+++ Balans/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user)
 
+
+
 class City(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=25)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        verbose_name_plural = 'cities'
--- Balans/views.py
+++ Balans/views.py
@@ -3,18 +3,40 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 import requests
-
-from .forms import Userregistarions
+from .models import City
+from .forms import Userregistarions, CityForm
 
 @login_required
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=3b212a2a8d80257d845ca625a25c1522'
-    city = 'las Vegas'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=3b212a2a8d80257d845ca625a25c1522'
+    city = 'Las Vegas'
 
-    city_weather = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    print(city_weather)
-    return render(request, 'user/index.html')
+    form = CityForm
+
+    if request.method == 'POST':  # only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save()
+
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(
+            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+
+    context = {'weather': weather_data}
+
+    return render(request, 'user/index.html', context)
 
 def register(request):
     if request.method == 'POST':
